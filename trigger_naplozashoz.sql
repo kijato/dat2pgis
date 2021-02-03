46240ea8fd7ffe4332b0316fb97755bfa5d448c7
kijato <kijato@gmail.com>
2021-02-03 01:20:38 +0100

    Create trigger_naplozashoz.sql

---
+++ trigger_naplozashoz.sql
@@ -0,0 +1,61 @@
+-- DROP TABLE meta.teszt;
+CREATE TABLE meta.teszt
+(
+  id serial NOT NULL,
+  nev text,
+  ertek numeric,
+  CONSTRAINT pk_teszt PRIMARY KEY (id)
+)
+WITH ( OIDS=FALSE);
+
+ALTER TABLE meta.teszt OWNER TO postgres;
+
+
+-- DROP TABLE meta.teszt_naplo;
+CREATE TABLE meta.teszt_naplo
+(
+  naplo_id integer NOT NULL DEFAULT nextval('meta.teszt_naplo_azonosito_seq'::regclass),
+  id integer,
+  nev text,
+  ertek numeric,
+  mu character varying(10),
+  md timestamp without time zone,
+  CONSTRAINT pk_teszt_naplo PRIMARY KEY (naplo_id),
+  CONSTRAINT fk_teszt_naplo FOREIGN KEY (id) REFERENCES meta.teszt (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH ( OIDS=FALSE );
+
+ALTER TABLE meta.teszt_naplo OWNER TO postgres;
+
+-- DROP INDEX meta.fki_teszt_naplo;
+CREATE INDEX fki_teszt_naplo ON meta.teszt_naplo USING btree (id);
+
+
+-- DROP FUNCTION meta.teszt_naplo();
+CREATE OR REPLACE FUNCTION meta.teszt_naplo()
+  RETURNS trigger AS
+$BODY$
+    BEGIN
+        IF (TG_OP = 'DELETE') THEN
+            --INSERT INTO emp_audit SELECT 'D', now(), user, OLD.*;
+            insert into meta.teszt_naplo (id,nev,ertek,mu,md) select OLD.*, user, now();
+        ELSIF (TG_OP = 'UPDATE') THEN
+            --INSERT INTO emp_audit SELECT 'U', now(), user, NEW.*;
+            insert into meta.teszt_naplo (id,nev,ertek,mu,md) select OLD.*, user, now();
+        ELSIF (TG_OP = 'INSERT') THEN
+            --INSERT INTO emp_audit SELECT 'I', now(), user, NEW.*;
+        END IF;
+        RETURN NULL; -- result is ignored since this is an AFTER trigger
+    END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION meta.teszt_naplo()
+  OWNER TO postgres;
+
+-- DROP TRIGGER u_naplo ON meta.teszt;
+CREATE TRIGGER u_naplo
+  AFTER UPDATE
+  ON meta.teszt
+  FOR EACH ROW
+  EXECUTE PROCEDURE meta.teszt_naplo();
